daa323c72233f26d4be246fea19dbc19ef948939
Taha GHAILAN <taha.ghailan17@gmail.com>
2024-07-27 19:13:55 +0100

    first commit

---
+++ JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/JeyahShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeyah.jeyahshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeyahShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeyahShopApiApplication.class, args);
+    }
+
+}
---
+++ JeyahShopAPI/src/test/java/com/jeyah/jeyahshopapi/JeyahShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeyah.jeyahshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeyahShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
