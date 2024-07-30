76901d974ab26b74c4f9351ccfc3d91c4f3e2b4d
Taha GHAILAN <taha.ghailan17@gmail.com>
2024-07-30 16:01:26 +0100

    Added Role based authorization using Keycloak (JwtAuthConverter class, securityFilterChain configuration related to keycloak

---
+++ JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/security/JwtAuthConverter.java
@@ -0,0 +1,2 @@
+package com.jeyah.jeyahshopapi.security;public class JwtAuthConverter {
+}
---
+++ JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.jeyah.jeyahshopapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(HttpSecurity::csrf)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
