a0903b7d284b2ec61fc55689cfa2c31a82b7dd14
Taha GHAILAN <taha.ghailan17@gmail.com>
2024-07-30 22:33:56 +0100

    Created the Angular project, Added the role based authorization using Keycloak, Created the Token entity and its repository

--- JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/MainController.java
+++ JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/MainController.java
@@ -1,2 +1,35 @@
-package com.jeyah.jeyahshopapi;public class MainController {
+package com.jeyah.jeyahshopapi;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("keycloak")
+public class MainController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello World";
+    }
+
+    @GetMapping("user/hello")
+    public String helloUser(){
+        return "Hello User";
+    }
+
+    @GetMapping("/manager/hello")
+    @PreAuthorize("hasRole('JeyahShop_manager')")
+    public String helloManager(){
+        return "Hello Manager";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('JeyahShop_admin')")
+    public String helloAdmin(){
+        return "Hello Admin";
+    }
 }
--- JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/role/Role.java
+++ JeyahShopAPI/src/main/java/com/jeyah/jeyahshopapi/role/Role.java
@@ -1,2 +1,41 @@
-package com.jeyah.jeyahshopapi.role;public class Role {
+package com.jeyah.jeyahshopapi.role;
+
+import com.jeyah.jeyahshopapi.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue
+    private String id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime creationDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
 }
