f091ef1ec86366a1cd5e8a1c59d972dd64d02418
Taha GHAILAN <taha.ghailan17@gmail.com>
2024-09-02 14:27:30 +0100

    Added the welcome component

--- JeyahShopUI/src/app/app.routes.ts
+++ JeyahShopUI/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {AppComponent} from "./app.component";
+import {WelcomeComponent} from "./pages/welcome/welcome.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'public/app-component',
+    component: AppComponent
+  },
+  {
+    path:'',
+    redirectTo:'public/welcome',
+    pathMatch:'full'
+  },
+  {
+    path:'public/welcome',
+    component: WelcomeComponent
+  }
+];
