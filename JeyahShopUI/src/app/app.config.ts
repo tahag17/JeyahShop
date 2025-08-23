6f37aeec66d10a849e31585340d44cb745dec127
Taha GHAILAN <taha.ghailan17@gmail.com>
2025-08-23 12:12:57 +0100

    Removed or commented out any file, import related to keycloak

--- JeyahShopUI/src/app/app.config.ts
+++ JeyahShopUI/src/app/app.config.ts
@@ -1,36 +1,36 @@
-import {APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
-import {KeycloakService} from "./services/keycloak/keycloak.service";
-import {HttpClient} from "@angular/common/http";
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+// import {KeycloakService} from "./services/keycloak/keycloak.service";
+import { HttpClient } from '@angular/common/http';
 
-export function kcFactory(kcService: KeycloakService) {
-  return () => kcService.init();
-  // return () => true;
+// export function kcFactory(kcService: KeycloakService) {
+//   return () => kcService.init();
+//   // return () => true;
 
-}
+// }
 
 export const appConfig: ApplicationConfig = {
   providers: [
-
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({ eventCoalescing: true }),
 
     provideRouter(routes),
 
     provideClientHydration(),
 
-    {
-      provide: APP_INITIALIZER,
-      deps: [KeycloakService],
-      useFactory: kcFactory,
-      multi: true
-    },
-
-    HttpClient
-
-
-
-  ]
+    //,
+    // {
+    //   provide: APP_INITIALIZER,
+    //   deps: [KeycloakService],
+    //   useFactory: kcFactory,
+    //   multi: true
+    // }
+    HttpClient,
+  ],
 };
--- JeyahShopUI/src/app/app.routes.ts
+++ JeyahShopUI/src/app/app.routes.ts
@@ -1,25 +1,24 @@
-import {Routes} from '@angular/router';
-import {LoginComponent} from "./pages/login/login.component";
-import {AppComponent} from "./app.component";
-import {AuthGuardService} from "./services/guard/auth-guard.service";
-import {WelcomeComponent} from "./pages/welcome/welcome.component";
-import {AboutComponent} from "./pages/about/about.component";
-import {ServicesComponent} from "./pages/services/services.component";
-import {ContactComponent} from "./pages/contact/contact.component";
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    canActivate: [AuthGuardService]
+    // canActivate: [AuthGuardService]
   },
   {
     path: '',
     redirectTo: 'public/welcome',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
-  {path: 'public/welcome', component: WelcomeComponent},
-  {path: 'public/about', component: AboutComponent},
-  {path: 'public/services', component: ServicesComponent},
-  {path: 'public/contact', component: ContactComponent},
+  { path: 'public/welcome', component: WelcomeComponent },
+  { path: 'public/about', component: AboutComponent },
+  { path: 'public/services', component: ServicesComponent },
+  { path: 'public/contact', component: ContactComponent },
 ];
