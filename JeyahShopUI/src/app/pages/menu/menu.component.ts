c35eb118bfed9aeb014b2f226c81bf4cfbefc5a3
Taha GHAILAN <taha.ghailan17@gmail.com>
2025-08-31 12:04:03 +0100

    Chose the color pallete, fonts, and added the dark/light mode toggle

--- JeyahShopUI/src/app/pages/menu/menu.component.ts
+++ JeyahShopUI/src/app/pages/menu/menu.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
--- JeyahShopUI/src/app/pages/welcome/welcome.component.ts
+++ JeyahShopUI/src/app/pages/welcome/welcome.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import {ServicesComponent} from "../services/services.component";
-import {RouterLink} from "@angular/router";
+import { ServicesComponent } from '../services/services.component';
+import { RouterLink } from '@angular/router';
+import { DarkModeToggleComponent } from '../../shared/dark-mode-toggle/dark-mode-toggle.component';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink, DarkModeToggleComponent],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
-
-}
+export class WelcomeComponent {}
